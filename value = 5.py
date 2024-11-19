baf16bca1d52a6d15973531f5876b31ae745b6e9
KENT <meshsize@gmail.com>
2024-11-19 11:20:36 +0900

    파이썬의 클래스 연습 완료

---
+++ value = 5.py
@@ -0,0 +1,10 @@
+class Person:
+    def ___init___(self):
+        self.name = "default name"
+        
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+p1 = Person()
+p1.print()
+
